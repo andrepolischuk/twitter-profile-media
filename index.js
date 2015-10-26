028b1a9b8f3b0b64b057b2e1bf5a6d2fb0c373f9
andrepolischuk <andre.polischuk@gmail.com>
2015-10-27 00:26:31 +0300

    Add get-twitter-info, fix #3

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-import Twitter from 'twitter';
+import info from 'get-twitter-info';
 
 export default (tokens, name, fn) => {
-  const client = new Twitter(tokens);
-  const options = {screen_name: name};
-
-  client.get('/users/show.json', options, (err, res) => {
+  info(tokens, name, (err, res) => {
     if (err) return fn(err);
     const image = res.profile_image_url_https.replace(/_normal/, '');
     const banner = res.profile_banner_url;
