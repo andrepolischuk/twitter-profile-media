42e7a77578430f177896146178c55a31d792fe0f
andrepolischuk <andre.polischuk@gmail.com>
2015-10-27 00:44:16 +0300

    Add ava instead of tape

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-import test from 'tape';
+import test from 'ava';
 import tokens from 'twitter-tokens';
 import media from './index';
 
@@ -6,7 +6,7 @@ test('should get image and banner', t => {
   t.plan(3);
 
   media(tokens, 'twitter', (err, {image, banner}) => {
-    t.ok(!err);
+    t.ifError(err);
     t.ok(image.length);
     t.ok(banner.length);
   });
@@ -16,7 +16,7 @@ test('should get image only', t => {
   t.plan(3);
 
   media(tokens, 'andrepolischuk', (err, {image, banner}) => {
-    t.ok(!err);
+    t.ifError(err);
     t.ok(image.length);
     t.notOk(banner);
   });
