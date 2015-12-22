6989f8f727fce12eb102b90be1159595339c22ae
andrepolischuk <andre.polischuk@gmail.com>
2015-12-22 15:47:06 +0300

    Promisify, fix #4

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import info from 'get-twitter-info';
+import Promise from 'pinkie-promise';
 
-export default (tokens, name, fn) => {
-  info(tokens, name, (err, res) => {
-    if (err) return fn(err);
-    const image = res.profile_image_url_https.replace(/_normal/, '');
-    const banner = res.profile_banner_url;
-    fn(err, {image, banner});
+export default (tokens, name) => {
+  return new Promise((resolve, reject) => {
+    info(tokens, name, (err, res) => {
+      if (err) return reject(err);
+      const image = res.profile_image_url_https.replace(/_normal/, '');
+      const banner = res.profile_banner_url;
+      resolve({image, banner});
+    });
   });
 };
--- test.js
+++ test.js
@@ -2,20 +2,14 @@ import test from 'ava';
 import tokens from 'twitter-tokens';
 import media from './index.es5';
 
-test.cb('should get image and banner', t => {
-  media(tokens, 'twitter', (err, {image, banner}) => {
-    t.ifError(err);
-    t.ok(image.length);
-    t.ok(banner.length);
-    t.end();
-  });
+test('get image and banner', async t => {
+  const {image, banner} = await media(tokens, 'twitter');
+  t.ok(image.length);
+  t.ok(banner.length);
 });
 
-test.cb('should get image only', t => {
-  media(tokens, 'andrepolischuk', (err, {image, banner}) => {
-    t.ifError(err);
-    t.ok(image.length);
-    t.notOk(banner);
-    t.end();
-  });
+test('get image only', async t => {
+  const {image, banner} = await media(tokens, 'andrepolischuk');
+  t.ok(image.length);
+  t.notOk(banner);
 });
