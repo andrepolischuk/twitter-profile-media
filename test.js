280d9e72e11a69550d002e16dbe26086b681829c
andrepolischuk <andre.polischuk@gmail.com>
2015-12-22 15:29:47 +0300

    Bump deps

--- test.js
+++ test.js
@@ -1,23 +1,21 @@
 import test from 'ava';
 import tokens from 'twitter-tokens';
-import media from './index';
+import media from './index.es5';
 
-test('should get image and banner', t => {
-  t.plan(3);
-
+test.cb('should get image and banner', t => {
   media(tokens, 'twitter', (err, {image, banner}) => {
     t.ifError(err);
     t.ok(image.length);
     t.ok(banner.length);
+    t.end();
   });
 });
 
-test('should get image only', t => {
-  t.plan(3);
-
+test.cb('should get image only', t => {
   media(tokens, 'andrepolischuk', (err, {image, banner}) => {
     t.ifError(err);
     t.ok(image.length);
     t.notOk(banner);
+    t.end();
   });
 });
