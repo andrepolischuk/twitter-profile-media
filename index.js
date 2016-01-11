240ba97160a99acfcb50455ceeb52be6f4941e67
andrepolischuk <andre.polischuk@gmail.com>
2016-01-11 12:13:07 +0300

    Tweaks

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 import info from 'get-twitter-info';
 import Promise from 'pinkie-promise';
 
-export default (tokens, name) => {
-  return new Promise((resolve, reject) => {
-    info(tokens, name, (err, res) => {
-      if (err) return reject(err);
-      const image = res.profile_image_url_https.replace(/_normal/, '');
-      const banner = res.profile_banner_url;
-      resolve({image, banner});
-    });
+export default (tokens, name) => new Promise((resolve, reject) => {
+  info(tokens, name, (err, res) => {
+    if (err) return reject(err);
+    const image = res.profile_image_url_https.replace(/_normal/, '');
+    const banner = res.profile_banner_url;
+    resolve({image, banner});
   });
-};
+});
