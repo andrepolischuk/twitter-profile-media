5409646ffcf9c39490f250fe0a35c21cd1465e51
andrepolischuk <andre.polischuk@gmail.com>
2016-02-04 16:21:17 +0300

    Bump get-twitter-info

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 import info from 'get-twitter-info';
-import Promise from 'pinkie-promise';
 
 export default function twitterProfileMedia(tokens, name) {
-  return new Promise((resolve, reject) => {
-    info(tokens, name, (err, res) => {
-      if (err) return reject(err);
-      const image = res.profile_image_url_https.replace(/_normal/, '');
-      const banner = res.profile_banner_url;
-      resolve({image, banner});
-    });
+  return info(tokens, name).then(res => {
+    const image = res.profile_image_url_https.replace(/_normal/, '');
+    const banner = res.profile_banner_url;
+    return {image, banner};
   });
 };
